46daba5fc2b2bbaf90e1b27bf2b75619bb21275e
HBell <sleehj96@naver.com>
2021-08-22 23:30:34 +0900

    11650 solved

---
+++ algorithm/baekjoon/11650_sorting_coord.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11650
+import sys
+
+N = int(sys.stdin.readline())
+
+coord_list = []
+for _ in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    coord_list.append([x, y])
+
+coord_list.sort(key=lambda x: (x[0], x[1]))
+
+for i in range(N):
+    print(*coord_list[i])
