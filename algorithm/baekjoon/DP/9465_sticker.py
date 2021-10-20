bd60a18d348b6c97b944fae3850ad869c9770ead
HBell <sleehj96@naver.com>
2021-10-20 19:52:44 +0900

    11055 solved

---
+++ algorithm/baekjoon/DP/9465_sticker.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/9465
+# Unsolved
+
+for _ in range(int(input())):
+    n = int(input())
+
+    stickers = [list(map(int, input().split())) for _ in range(2)]
+
+    print(stickers)
