a4d177ffece9e66ea3c28ddd92a5074440d5e040
HBell <sleehj96@naver.com>
2021-11-10 20:14:01 +0900

    17609 solved

--- algorithm/baekjoon/string/17609_palindrome.py
+++ algorithm/baekjoon/string/17609_palindrome.py
@@ -5,26 +5,20 @@ def pal(s: str, chance):
     length = len(s)
 
     for i in range(length//2):
-        if s[i] != s[-(i+1)]:
+        if s[i] != s[length-(i+1)]:
             if chance:
-                print(s[:i] + s[i+1:])
                 tmp1 = pal(s[:i] + s[i+1:], False)
                 if not tmp1:
                     return 1
-
-                print(s[:-i-1] + s[-i:])
-                tmp2 = pal(s[:-(i+1)] + s[-i:], False)
+                tmp2 = pal(s[:length-(i+1)] + s[length-i:], False)
                 if not (tmp2):
                     return 1
-            else:
-                return 2
+            return 2
     else:
         return 0
 
 
-T = int(input())
-
-for _ in range(T):
+for _ in range(int(input())):
     s = input()
 
     print(pal(s, True))
